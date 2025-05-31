b1886a2a931c66a622e5080e79caa3ccac5f7047
keerthish waran <durai.s@spotft.com>
2025-05-31 23:07:11 +0530

    publisg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/landingpage/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // allows access via local network (like ngrok)
